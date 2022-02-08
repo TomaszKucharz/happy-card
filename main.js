378a6009d81396941f6d5dd3c1c224c2271297f9
Tomasz Kucharz <tkucharz@frontendwebsite.com>
2022-02-08 02:48:59 +0100

    8.02.2022

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const cardsArray = [...document.querySelectorAll(`.card`)];
+const cardLayerArray = [...document.querySelectorAll(`.cardLayer`)];
 const buttonOne = document.getElementById(`btn-1`);
 const buttonTwo = document.getElementById(`btn-2`);
 const buttonThree = document.getElementById(`btn-3`);
 const clear = document.querySelector(`.clear`);
 const start = document.querySelector(`.start`);
 const buttonsArrays = [buttonOne, buttonTwo, buttonThree];
+let F = 0;
 
 const blockOtherButtons = () => {
-	buttonsArrays.forEach((el) => {
+	buttonsArrays.forEach((el, index) => {
 		if (el.textContent !== 'X') {
 			el.classList.add(`off`);
 			el.setAttribute('disabled', true);
+		} else if (el.textContent === 'X') {
+			F = index;
 		}
 	});
 };
@@ -21,9 +25,8 @@ const showSign = (e) => {
 	if (e.target.textContent === '') {
 		e.target.textContent = 'X';
 		start.style.visibility = 'visible';
-
 		e.target.classList.add('on');
-		console.log(buttonsArrays.indexOf(e.target));
+		// console.log(buttonsArrays.indexOf(e.target));
 		blockOtherButtons();
 	} else {
 		return alert('wyczyść klawisze');
@@ -38,8 +41,14 @@ start.addEventListener(`click`, () => {
 	const wynik = cardsArray[Math.floor(Math.random() * cardsArray.length)];
 	console.log(cardsArray.indexOf(wynik));
 	wynik.classList.add('cardActive');
+	cardLayerArray[cardsArray.indexOf(wynik)].classList.add(`moveCard`);
 	start.style.visibility = 'hidden';
 	clear.style.visibility = 'visible';
+	if (F === cardsArray.indexOf(wynik)) {
+		console.log(`YOU WIN`);
+	} else {
+		console.log(`YOU LOST`);
+	}
 });
 
 clear.addEventListener(`click`, () => {
@@ -51,6 +60,9 @@ clear.addEventListener(`click`, () => {
 	});
 	cardsArray.forEach((el) => {
 		el.classList.remove('cardActive');
+	});
+	cardLayerArray.forEach((el) => {
+		el.classList.remove(`moveCard`);
 	});
 
 	clear.style.visibility = 'hidden';
